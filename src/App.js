6077c32e9cc25e292caf8be23aa44a940b8062a4
jenniferlindberg <jinjin.lindberg@gmail.com>
2021-11-21 16:13:25 +0100

    feat: start with header and presentation
    * add brief profile
    * add  background image
    * add scroll button
    * update theme file
    
    Issue 3, Issue #4, Issue #5

--- src/App.js
+++ src/App.js
@@ -1,22 +1,76 @@
 import logo from "./images/logo.png";
 import "./App.css";
 import Typography from "@mui/material/Typography";
+import React, { useRef } from "react";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  pageFooter: {
+    "&::selection": {
+      // this is to refer to the prop provided by M-UI
+      backgroundColor: theme.palette.secondary.dark,
+      color: "white",
+      fontWeight: 600,
+    },
+  },
+}));
 
 function App() {
+  const titleRef = useRef();
+  const classes = useStyles();
+
+  function handleBackClick() {
+    titleRef.current.scrollIntoView({ behavior: "smooth" });
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <Typography color="primary.light">Hello</Typography>
-        <Typography color="primary">Hello</Typography>
-        <Typography color="primary.dark">Hello</Typography>
+        <img src={logo} className="App-logo" alt="logo" />
+        <Typography
+          fontSize="fontSize.large"
+          fontFamily="fontFamily.georgia"
+          fontStyle="fontStyle.italic"
+        >
+          Jennifer Lindberg
+        </Typography>
+        <Typography
+          fontSize="fontSize"
+          fontFamily="fontFamily.georgia"
+          sx={{ letterSpacing: 6 }}
+        >
+          Software Engineer
+        </Typography>
+        <Typography
+          fontSize="fontSize.small"
+          fontFamily="fontFamily.georgia"
+          sx={{ letterSpacing: 4 }}
+          className="description"
+        >
+          Passionate about building responsive websites
+        </Typography>
+      </header>
+      <div className="presentation">
+        <Typography
+          color="primary.light"
+          ref={titleRef}
+          fontSize="fontSize.small"
+          className={classes.pageFooter}
+        >
+          Hello
+        </Typography>
+        <Typography color="primary" fontSize="fontSize.main">
+          Hello
+        </Typography>
+        <Typography color="primary.dark" fontSize="fontSize.large">
+          Hello
+        </Typography>
 
         <Typography color="secondary.light">Hello</Typography>
         <Typography color="secondary">Hello</Typography>
         <Typography color="secondary.dark">Hello</Typography>
-
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Homepage - Work Profile Jennifer Lindberg</p>
-      </header>
+        <button onClick={handleBackClick}>Back</button>
+      </div>
     </div>
   );
 }
--- src/theme.js
+++ src/theme.js
@@ -19,6 +19,22 @@ const theme = createTheme({
       main: red.A400,
     },
   },
+  typography: {
+    // In Chinese and Japanese the characters are usually larger,
+    // so a smaller fontsize may be appropriate.
+    fontSize: {
+      small: "calc(10px + 0.8vmin)",
+      main: "calc(10px + 5vmin)",
+      large: "calc(10px + 10vmin)",
+    },
+    fontFamily: {
+      georgia: `"georgia", "palatino", "book antiqua", "palatino linotype", serif`,
+      lobster: `"lobster", "cursive"`,
+    },
+    fontStyle: {
+      italic: "italic",
+    },
+  },
 });
 
 export default theme;
